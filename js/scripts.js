18f558692baaca37ca0a9c74c7beedc2031c79a1
Tyler Arehart <tarehart@gmail.com>
2022-05-08 15:34:39 -0700

    Removing the last of the Ram and Antara text.

--- js/scripts.js
+++ js/scripts.js
@@ -149,23 +149,19 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Kora and Tyler's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('July 29, 2022 15:00'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
-
-            // You can also choose to set an end time
-            // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            duration: 300,
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '8718 314th Ave SE, Issaquah, WA 98027',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "Can't wait to see you at the wedding! See https://tarehart.github.io/wedding-website/ for details."
         }
     });
 
